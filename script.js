5974c63d63718536aeafa29d4f81ee66861fddfc
Ramsha <fnuramsha@gmail.com>
2024-05-08 00:32:09 +0200

    implemented functionality using await

--- script.js
+++ script.js
@@ -37,20 +37,52 @@ function geoPosition() {
 }
 
 const whereAMI = async function () {
-  // geocation
-  const pos = await geoPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
-  //   reverse geooding
-  const geoApi = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-  const dataGeo = await geoApi.json();
-  console.log(dataGeo);
-  // Country data
-  const res = await fetch(
-    `https://restcountries.com/v3.1/name/${dataGeo.country}`
-  );
-  const data = await res.json();
-  console.log(data);
-  renderCountry(data[0]);
+  try {
+    // geocation
+    const pos = await geoPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
+    //   reverse geooding
+    const geoApi = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!geoApi.ok) {
+      throw new Error("Problem getting location data");
+    }
+    const dataGeo = await geoApi.json();
+
+    // Country data
+    const res = await fetch(
+      `https://restcountries.com/v3.1/name/${dataGeo.country}`
+    );
+
+    if (!res.ok) {
+      throw new Error("Problem getting country data");
+    }
+    const data = await res.json();
+    renderCountry(data[0]);
+    return `You're in ${dataGeo.city} and ${dataGeo.country}`;
+  } catch (err) {
+    console.error(`${err}`);
+    renderError(`${err.message}`);
+    // Reject the prmise returned from async function
+    throw err;
+  }
 };
-whereAMI();
-console.log("First");
+console.log("1. You'll get the location");
+// const city = whereAMI();
+// console.log(city);
+// whereAMI()
+//   .then((city) => console.log(`2: ${city}`))
+//   .catch((err) => console.error(`2: ${err.message}`))
+//   .finally(() => console.log("3.Checking the last log"));
+// console.log(whereAMI());
+
+// Conversion of .then to await promises
+
+(async function () {
+  try {
+    const city = await whereAMI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message}`);
+  }
+  console.log("3. You got the location");
+})();
